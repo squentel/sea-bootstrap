af777b913a4f3b158c6535796d4d6acfda7a660f
squentel <30870132+squentel@users.noreply.github.com>
2023-06-12 13:00:57 +0200

    Create bootstrapdocker.sh

---
+++ bootstrapdocker.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+echo "======= DOCKER INSTALLATION"
+sudo apt-get remove docker docker-engine docker.io containerd runc
+
+sudo apt-get update && sudo apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg \
+    jq \
+    lsb-release
+
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+# curl -fsSL https://download.docker.com/linux/ubuntu/gpg | \
+#     sudo gpg --dearmor -o \
+#     /usr/share/keyrings/docker-archive-keyring.gpg
+
+# echo \
+#   "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+#   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt-get update && sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+
+echo "======= DOCKER : ADD $USER to docker usergroup"
+sudo usermod -a -G docker "$USER"
+
